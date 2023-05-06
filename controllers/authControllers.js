ec4592cb0fc8608b1a26f7846d38f7d176e5fe36
Sajjad <65295591+Sajjad-Mi@users.noreply.github.com>
2023-05-06 20:34:18 +0430

    refactor: implement authentication using sequelize

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,111 +1,94 @@
-const fs = require("fs");
 const bcrypt = require("bcrypt");
-const jwt = require('jsonwebtoken');
-const validator = require('validator');
+const jwt = require("jsonwebtoken");
+const validator = require("validator");
 
+const db = require("../model");
+const User = db.users;
+const createToken = (user_id, name) => {
+  return jwt.sign({ user_id, name }, process.env.JWT_SECRET, {
+    expiresIn: "2 days",
+  });
+};
+const pre = async function (pass) {
+  const salt = await bcrypt.genSalt();
+  return await bcrypt.hash(pass, salt);
+};
+const validpass = (password) => {
+  if (password.length < 6) {
+    throw Error("Your password shoud be at least 6 char");
+  }
+};
+const validEmail = (email) => {
+  if (!validator.isEmail(email)) {
+    throw Error("You don't enter a valid email");
+  } 
+};
+module.exports.signup_get = (req, res) => {
+  res.render("SignUp");
+};
 
-const createToken = (userName, name)=>{
-    return jwt.sign({ userName, name}, 'sajjad', {
-        expiresIn: "2 days"
-      });
-}
-const pre = async function(pass){                                               //function for hashing the password
-    const salt = await bcrypt.genSalt();
-    const data = await bcrypt.hash(pass, salt);
-    return data;
-}
-const validpass = (password)=>{
-    if(password.length < 6){
-        throw Error("Your password shoud be at least 6 char")
+module.exports.signup_post = async (req, res) => {
+  try {
+    validEmail(req.body.email);
+    validpass(req.body.password); 
+    const hashedPass = await pre(JSON.stringify(req.body.password)); 
+
+    const user = {
+      name: req.body.username,
+      email: req.body.email,
+      password: hashedPass,
+    };
+    const newUser = await User.create(user);
+    console.log(newUser.toJSON());
+    const token = createToken(newUser.user_id, newUser.name);
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      maxAge: 2 * 24 * 60 * 60 * 1000,
+    });
+    const userEmail = newUser.email;
+    res.status(201).json({ user: userEmail, error: "" });
+  } catch (error) {
+    console.log(error.message);
+    error = error.message;
+    res.status(400).json({ error });
+  }
+};
+
+module.exports.login_get = (req, res) => {
+  res.render("login");
+};
+
+module.exports.login_post = async (req, res) => {
+  console.log(req.body);
+  try {
+    const user = await User.findOne({ where: { email: req.body.email } });
+
+    if (user != null) {
+      const isValid = await bcrypt.compare(
+        JSON.stringify(req.body.password),
+        user.password
+      ); 
+      if (isValid) {
+        console.log("your email and password is correct");
+        const token = createToken(user.user_id, user.name);
+        res.cookie("jwt", token, {
+          httpOnly: true,
+          maxAge: 2 * 24 * 60 * 60 * 1000,
+        });
+        res.status(200).json({ user: user.email, error: "" });
+      } else {
+        throw Error("your password or email is wrong");
+      }
+    } else {
+      throw Error("your password or email is wrong");
     }
-}
-const validEmail = (email)=>{
-    if(!validator.isEmail(email)){
-        throw Error("You don't enter a valid email")
-    } else{
-        if(fs.existsSync(`./data/${email}`)){
-            throw Error("This email is already sign up")
-        }
-    }
-    
-}
-module.exports.signup_get = (req , res) =>{
-    res.render("SignUp")
-}
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
-module.exports.signup_post = async(req , res) =>{
-    try {
-        validEmail(req.body.email)
-        validpass(req.body.password)                                                //throw an error if the input isn't valid
-        const pass =await pre(JSON.stringify(req.body.password));                   //hashing
-                                                      
-        const user={username: req.body.username, email:req.body.email, password:pass};
-        await new Promise((resolve, reject)=>{            
-            if(!fs.existsSync('./data')){
-                fs.mkdir('./data', (err)=>{
-                    console.log(err)
-                })
-            }
-            fs.mkdir(`./data/${req.body.email}`, (err)=>{
-                console.log(err)
-                if(!err){
-                    resolve("made")
-                }
-            })
-       })
-        
-        fs.writeFile(`./data/${req.body.email}/info.txt`, JSON.stringify(user), (err)=>{          //save the hash value email in a file
-            console.log(err);
-        })
-        const token = createToken(req.body.email, req.body.username);
-        res.cookie('jwt', token, { httpOnly: true, maxAge: 2* 24 *60 *60* 1000 });
-        const userEmail = req.body.email;
-        res.status(201).json({ user: userEmail , error: ""});
-    } catch (error) {
-        console.log(error.message);
-        error = error.message;
-        res.status(400).json({ error });
-    }
-}
 
-module.exports.login_get = (req , res) =>{
-    res.render("login")
-}
-
-module.exports.login_post =async (req , res) =>{
-    console.log(req.body)
-    try {
-        if(fs.existsSync(`./data/${req.body.email}`)){                                           //check if the user exist else throw error
-            const user = JSON.parse(await new Promise((resolve, reject)=>{
-                fs.readFile(`./data/${req.body.email}/info.txt`, "utf8", (err, data)=>{                  //read the user info and save it in user const
-                    if(data){
-                        resolve(data);
-                    } 
-                    if(err){
-                        console.log(err);
-                    }
-                })
-            }))
-            const isValid = await bcrypt.compare(JSON.stringify(req.body.password), user.password);                         //check the enterd password with database
-            if(isValid){
-                console.log("your email and password is correct")
-                const token = createToken(user.email, user.username);
-                res.cookie('jwt', token, { httpOnly: true, maxAge: 2 * 24 * 60 * 60 * 1000 });
-                res.status(200).json({ user: user.email, error: "" });
-        
-            }else{
-                throw Error("your password or email is wrong")
-            } 
-        } else {
-            throw Error("your password or email is wrong")
-        }          
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-     
-}
-
-module.exports.logout_get = (req , res) =>{
-    res.cookie('jwt', '', {maxAge: 1})
-    res.redirect('/')
-}
+module.exports.logout_get = (req, res) => {
+  res.cookie("jwt", "", { maxAge: 1 });
+  res.redirect("/");
+};
